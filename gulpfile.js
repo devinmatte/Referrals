32a2d799f5649eb7b5379bfcd316c34157678a8a
Devin Matte <devinmatte@gmail.com>
2019-11-15 10:57:53 -0500

    Convert to React

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const gulp = require('gulp');
+const realFavicon = require('gulp-real-favicon');
+
+// File where the favicon markups are stored
+const FAVICON_DATA_FILE = 'faviconData.json';
+
+// Generate the icons. This task takes a few seconds to complete.
+// You should run it at least once to create the icons. Then,
+// you should run it whenever RealFaviconGenerator updates its
+// package (see the check-for-favicon-update task below).
+gulp.task('generate-favicon', (done) => {
+    realFavicon.generateFavicon({
+        masterPicture: './src/logo.svg',
+        dest: './public',
+        iconsPath: '/',
+        design: {
+            ios: {
+                pictureAspect: 'backgroundAndMargin',
+                backgroundColor: '#ffffff',
+                margin: '35%',
+                assets: {
+                    ios6AndPriorIcons: false,
+                    ios7AndLaterIcons: false,
+                    precomposedIcons: false,
+                    declareOnlyDefaultIcon: true
+                }
+            },
+            desktopBrowser: {},
+            windows: {
+                pictureAspect: 'whiteSilhouette',
+                backgroundColor: '#b0197e',
+                onConflict: 'override',
+                assets: {
+                    windows80Ie10Tile: true,
+                    windows10Ie11EdgeTiles: {
+                        small: true,
+                        medium: true,
+                        big: true,
+                        rectangle: true
+                    }
+                }
+            },
+            androidChrome: {
+                pictureAspect: 'noChange',
+                themeColor: '#b0197e',
+                manifest: {
+                    display: 'standalone',
+                    orientation: 'notSet',
+                    onConflict: 'override',
+                    declared: true
+                },
+                assets: {
+                    legacyIcon: false,
+                    lowResolutionIcons: false
+                }
+            },
+            safariPinnedTab: {
+                pictureAspect: 'silhouette',
+                themeColor: '#b0197e'
+            }
+        },
+        settings: {
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false,
+            readmeFile: false,
+            htmlCodeFile: false,
+            usePathAsIs: false
+        },
+        markupFile: FAVICON_DATA_FILE
+    }, function () {
+        done();
+    });
+});
+
+gulp.task('default', gulp.parallel('generate-favicon'));
---
+++ src/Product/Product.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../styles/main.scss'
+import '../styles/brands.scss'
+
+type ProductProps = {
+    product: string,
+    description: string,
+    link: string
+}
+
+const Product: React.FunctionComponent<ProductProps> = ({ product, description, link }) => {
+    const imageWebpSource = "images/products/webp/" + encodeURI(product) + ".webp"
+    const imagePngSource = "images/products/png/" + encodeURI(product) + ".png"
+    return (
+        <article className={product}>
+            <picture className="image">
+                <source srcSet={imageWebpSource} type="image/webp" />
+                <source srcSet={imagePngSource} type="image/png" />
+                <img src={imagePngSource} data-position="center" alt={product} />
+            </picture>
+            <a href={link}>
+                <h2>{product}</h2>
+                <div className="content">
+                    <p>{description}</p>
+                </div>
+            </a>
+        </article>
+    );
+}
+
+export default Product;
